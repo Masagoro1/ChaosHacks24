856c0b3490c1126954d613a8a60d901ceefbfd11
Cop1ous <cole.brad.mart@gmail.com>
2024-03-02 14:10:26 -0800

    Added Health Manager

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image healthBar;
+    public float healthAmount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+
+        if (healthAmount = 0) {
+            Application.ForceCrash;
+        }
+    }
+
+    public void TakeDamage(float damage) 
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    private void OnTriggerEnter2d(Collider2D other){
+        if(other.Tag == "Projectile"){
+            TakeDamage(20);
+        }
+    }
+}
